32cc253a524a77ea0860b6e0aa950d804cafcbac
박선준 <juiceyf11@gmail.com>
2024-02-23 17:38:47 +0900

    update: major update. add more function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// global variables
 var dateString = "";
 var charClass;
 var jsonHexa = {};
@@ -5,29 +6,52 @@ var coreInfo = [];
 var coreData = [];
 let iconData;
 let foundIdx;
+var _Name;
 
+// api variables
 const api_keys =
   "test_2785d54ef3df048746d128e9eddfa161b9c3b6b2dcc95833692f554efa53d830e1e63a06c989db87d9a7b82b19c139dc";
 var ocid = "";
 const url = "https://open.api.nexon.com/maplestory/";
 
-const button = document.getElementById("search_btn");
-button.addEventListener("click", function () {
-  var _Name = document.getElementById("name").value;
-  getDate();
-  getOcid(_Name);
+var nowCollapseEl = document.getElementById("result_page");
 
-  document.getElementById("name").value = "";
-});
+const search_btn = document.getElementById("search_btn");
+search_btn.addEventListener("click", search);
+
+const res_btn = document.getElementById("reset-btn");
+res_btn.addEventListener("click", reSet);
 
 function enterKey(e) {
-  var _Name = document.getElementById("name").value;
+  _Name = document.getElementById("name").value;
   const code = e.code;
   if (code === "Enter") {
-    getDate();
-    getOcid(_Name);
-    document.getElementById("name").value = "";
+    search();
   }
+}
+
+function reSet() {
+  var coreEl = document.querySelector(".now-cores");
+  var goalEl = document.querySelector(".goal-cores");
+  new bootstrap.Collapse(nowCollapseEl, { hidden: true });
+  coreEl.innerHTML = "";
+  goalEl.innerHTML = "";
+  ocid = "";
+  dateString = "";
+  jsonHexa = "";
+  coreInfo = "";
+  coreData = "";
+  iconData = "";
+  foundIdx = "";
+}
+
+function search() {
+  if (ocid !== "") {
+    reSet();
+  }
+  _Name = document.getElementById("name").value;
+  getDate();
+  getOcid(_Name);
 }
 
 function getDate() {
@@ -37,8 +61,8 @@ function getDate() {
   var day = ("0" + today.getDate()).slice(-2);
   dateString = "&date=" + year + "-" + month + "-" + (day - 1);
 }
-
 function getOcid(nickName) {
+  ocid = "";
   var urlString = url + "v1/id?character_name=" + nickName;
   fetch(urlString, {
     headers: {
@@ -52,6 +76,7 @@ function getOcid(nickName) {
     });
 }
 function getClassName() {
+  charClass = "";
   var urlString = url + "v1/character/basic?ocid=" + ocid + dateString;
   fetch(urlString, {
     headers: {
@@ -92,6 +117,7 @@ function getHexaInfo() {
 }
 
 function getSkillIcon() {
+  coreData = [];
   const sortData = {
     0: [
       "히어로",
@@ -2626,6 +2652,7 @@ function getSkillIcon() {
 
   console.log(iconData[foundIdx]);
   console.log(coreInfo);
+  console.log(coreData);
   for (let i = 0; i < 8; i++) {
     let data = iconData[foundIdx].skills;
     var inputData = [
@@ -2641,10 +2668,684 @@ function getSkillIcon() {
     for (let j = 0; j < 8; j++) {
       if (coreData[j][0] === coreInfo[i][0]) {
         coreData[j][1] = coreInfo[i][1];
-        // console.log(coreData[i]);
       } else {
       }
     }
   }
   console.log(coreData);
+  drawNowCore();
+}
+
+function drawNowCore() {
+  var coreEl = document.querySelector(".now-cores");
+  coreEl.innerHTML = "";
+
+  for (let index = 0; index < 8; index++) {
+    var inEl = document.createElement("div");
+    inEl.className = "col p-1";
+    inEl.innerHTML =
+      "<img src=" +
+      coreData[index][2] +
+      "><br>" +
+      coreData[index][0] +
+      "<br><span class='badge bg-primary text-white'> Lv." +
+      coreData[index][1] +
+      "</span>";
+    coreEl.appendChild(inEl);
+  }
+
+  new bootstrap.Collapse(nowCollapseEl, { show: true });
+  drawGoalCore();
+}
+
+function drawGoalCore() {
+  var goalEl = document.querySelector(".goal-cores");
+  goalEl.innerHTML = "";
+
+  for (let index = 0; index < 8; index++) {
+    var inEl = document.createElement("div");
+    inEl.className = "col p-1";
+    inEl.innerHTML =
+      "<img src=" +
+      coreData[index][2] +
+      "><br>" +
+      coreData[index][0] +
+      `<br><input class="small" type="number" step="1" max="30" min="${coreData[index][1]}" id="core${index}" value='${coreData[index][1]}'>`;
+    goalEl.appendChild(inEl);
+  }
+}
+document.getElementById("btn-calc").addEventListener("click", () => {
+  new bootstrap.Collapse(nowCollapseEl, { hidden: true });
+  drawCalcPage();
+});
+function drawCalcPage() {
+  const DB = {
+    title: "MapleStory Hexa Skills Requirment Items Calcualtor",
+    "Game Target Version": "KMS 1.2.386",
+    "KMS(T)": true,
+    "Latest Edit": "2024-02-14T14:00+09:00",
+    Cores: {
+      Common: {
+        name: "공용 코어",
+        data: [
+          {
+            Lv: 1,
+            Erda: 7,
+            Fragments: 125,
+          },
+          {
+            Lv: 2,
+            Erda: 2,
+            Fragments: 38,
+          },
+          {
+            Lv: 3,
+            Erda: 2,
+            Fragments: 44,
+          },
+          {
+            Lv: 4,
+            Erda: 2,
+            Fragments: 50,
+          },
+          {
+            Lv: 5,
+            Erda: 3,
+            Fragments: 57,
+          },
+          {
+            Lv: 6,
+            Erda: 3,
+            Fragments: 63,
+          },
+          {
+            Lv: 7,
+            Erda: 3,
+            Fragments: 69,
+          },
+          {
+            Lv: 8,
+            Erda: 5,
+            Fragments: 75,
+          },
+          {
+            Lv: 9,
+            Erda: 5,
+            Fragments: 82,
+          },
+          {
+            Lv: 10,
+            Erda: 14,
+            Fragments: 300,
+          },
+          {
+            Lv: 11,
+            Erda: 5,
+            Fragments: 110,
+          },
+          {
+            Lv: 12,
+            Erda: 5,
+            Fragments: 124,
+          },
+          {
+            Lv: 13,
+            Erda: 6,
+            Fragments: 138,
+          },
+          {
+            Lv: 14,
+            Erda: 6,
+            Fragments: 152,
+          },
+          {
+            Lv: 15,
+            Erda: 6,
+            Fragments: 165,
+          },
+          {
+            Lv: 16,
+            Erda: 6,
+            Fragments: 179,
+          },
+          {
+            Lv: 17,
+            Erda: 6,
+            Fragments: 193,
+          },
+          {
+            Lv: 18,
+            Erda: 6,
+            Fragments: 207,
+          },
+          {
+            Lv: 19,
+            Erda: 7,
+            Fragments: 220,
+          },
+          {
+            Lv: 20,
+            Erda: 17,
+            Fragments: 525,
+          },
+          {
+            Lv: 21,
+            Erda: 7,
+            Fragments: 234,
+          },
+          {
+            Lv: 22,
+            Erda: 7,
+            Fragments: 248,
+          },
+          {
+            Lv: 23,
+            Erda: 7,
+            Fragments: 262,
+          },
+          {
+            Lv: 24,
+            Erda: 7,
+            Fragments: 275,
+          },
+          {
+            Lv: 25,
+            Erda: 7,
+            Fragments: 289,
+          },
+          {
+            Lv: 26,
+            Erda: 9,
+            Fragments: 303,
+          },
+          {
+            Lv: 27,
+            Erda: 9,
+            Fragments: 317,
+          },
+          {
+            Lv: 28,
+            Erda: 9,
+            Fragments: 330,
+          },
+          {
+            Lv: 29,
+            Erda: 10,
+            Fragments: 344,
+          },
+          {
+            Lv: 30,
+            Erda: 20,
+            Fragments: 750,
+          },
+        ],
+      },
+      Mastery: {
+        name: "마스터리(4차) 코어",
+        data: [
+          {
+            Lv: 1,
+            Erda: 3,
+            Fragments: 50,
+          },
+          {
+            Lv: 2,
+            Erda: 1,
+            Fragments: 15,
+          },
+          {
+            Lv: 3,
+            Erda: 1,
+            Fragments: 18,
+          },
+          {
+            Lv: 4,
+            Erda: 1,
+            Fragments: 20,
+          },
+          {
+            Lv: 5,
+            Erda: 1,
+            Fragments: 23,
+          },
+          {
+            Lv: 6,
+            Erda: 1,
+            Fragments: 25,
+          },
+          {
+            Lv: 7,
+            Erda: 1,
+            Fragments: 28,
+          },
+          {
+            Lv: 8,
+            Erda: 2,
+            Fragments: 30,
+          },
+          {
+            Lv: 9,
+            Erda: 2,
+            Fragments: 33,
+          },
+          {
+            Lv: 10,
+            Erda: 5,
+            Fragments: 100,
+          },
+          {
+            Lv: 11,
+            Erda: 2,
+            Fragments: 40,
+          },
+          {
+            Lv: 12,
+            Erda: 2,
+            Fragments: 45,
+          },
+          {
+            Lv: 13,
+            Erda: 2,
+            Fragments: 50,
+          },
+          {
+            Lv: 14,
+            Erda: 2,
+            Fragments: 55,
+          },
+          {
+            Lv: 15,
+            Erda: 2,
+            Fragments: 60,
+          },
+          {
+            Lv: 16,
+            Erda: 2,
+            Fragments: 65,
+          },
+          {
+            Lv: 17,
+            Erda: 2,
+            Fragments: 70,
+          },
+          {
+            Lv: 18,
+            Erda: 2,
+            Fragments: 75,
+          },
+          {
+            Lv: 19,
+            Erda: 3,
+            Fragments: 80,
+          },
+          {
+            Lv: 20,
+            Erda: 8,
+            Fragments: 175,
+          },
+          {
+            Lv: 21,
+            Erda: 3,
+            Fragments: 85,
+          },
+          {
+            Lv: 22,
+            Erda: 3,
+            Fragments: 90,
+          },
+          {
+            Lv: 23,
+            Erda: 3,
+            Fragments: 95,
+          },
+          {
+            Lv: 24,
+            Erda: 3,
+            Fragments: 100,
+          },
+          {
+            Lv: 25,
+            Erda: 3,
+            Fragments: 105,
+          },
+          {
+            Lv: 26,
+            Erda: 3,
+            Fragments: 110,
+          },
+          {
+            Lv: 27,
+            Erda: 3,
+            Fragments: 115,
+          },
+          {
+            Lv: 28,
+            Erda: 3,
+            Fragments: 120,
+          },
+          {
+            Lv: 29,
+            Erda: 4,
+            Fragments: 125,
+          },
+          {
+            Lv: 30,
+            Erda: 10,
+            Fragments: 250,
+          },
+        ],
+      },
+      Boost: {
+        name: "강화(5차) 코어",
+        data: [
+          {
+            Lv: 1,
+            Erda: 4,
+            Fragments: 75,
+          },
+          {
+            Lv: 2,
+            Erda: 1,
+            Fragments: 23,
+          },
+          {
+            Lv: 3,
+            Erda: 1,
+            Fragments: 27,
+          },
+          {
+            Lv: 4,
+            Erda: 1,
+            Fragments: 30,
+          },
+          {
+            Lv: 5,
+            Erda: 2,
+            Fragments: 34,
+          },
+          {
+            Lv: 6,
+            Erda: 2,
+            Fragments: 38,
+          },
+          {
+            Lv: 7,
+            Erda: 2,
+            Fragments: 42,
+          },
+          {
+            Lv: 8,
+            Erda: 3,
+            Fragments: 45,
+          },
+          {
+            Lv: 9,
+            Erda: 3,
+            Fragments: 49,
+          },
+          {
+            Lv: 10,
+            Erda: 8,
+            Fragments: 150,
+          },
+          {
+            Lv: 11,
+            Erda: 3,
+            Fragments: 60,
+          },
+          {
+            Lv: 12,
+            Erda: 3,
+            Fragments: 68,
+          },
+          {
+            Lv: 13,
+            Erda: 3,
+            Fragments: 75,
+          },
+          {
+            Lv: 14,
+            Erda: 3,
+            Fragments: 83,
+          },
+          {
+            Lv: 15,
+            Erda: 3,
+            Fragments: 90,
+          },
+          {
+            Lv: 16,
+            Erda: 3,
+            Fragments: 98,
+          },
+          {
+            Lv: 17,
+            Erda: 3,
+            Fragments: 105,
+          },
+          {
+            Lv: 18,
+            Erda: 3,
+            Fragments: 113,
+          },
+          {
+            Lv: 19,
+            Erda: 4,
+            Fragments: 120,
+          },
+          {
+            Lv: 20,
+            Erda: 12,
+            Fragments: 263,
+          },
+          {
+            Lv: 21,
+            Erda: 4,
+            Fragments: 128,
+          },
+          {
+            Lv: 22,
+            Erda: 4,
+            Fragments: 135,
+          },
+          {
+            Lv: 23,
+            Erda: 4,
+            Fragments: 143,
+          },
+          {
+            Lv: 24,
+            Erda: 4,
+            Fragments: 150,
+          },
+          {
+            Lv: 25,
+            Erda: 4,
+            Fragments: 158,
+          },
+          {
+            Lv: 26,
+            Erda: 5,
+            Fragments: 165,
+          },
+          {
+            Lv: 27,
+            Erda: 5,
+            Fragments: 173,
+          },
+          {
+            Lv: 28,
+            Erda: 5,
+            Fragments: 180,
+          },
+          {
+            Lv: 29,
+            Erda: 6,
+            Fragments: 188,
+          },
+          {
+            Lv: 30,
+            Erda: 15,
+            Fragments: 375,
+          },
+        ],
+      },
+      Skills: {
+        name: "스킬(6차 / 오리진) 코어",
+        data: [
+          {
+            Lv: 1,
+            Erda: 0,
+            Fragments: 0,
+          },
+          {
+            Lv: 2,
+            Erda: 1,
+            Fragments: 30,
+          },
+          {
+            Lv: 3,
+            Erda: 1,
+            Fragments: 35,
+          },
+          {
+            Lv: 4,
+            Erda: 1,
+            Fragments: 40,
+          },
+          {
+            Lv: 5,
+            Erda: 2,
+            Fragments: 45,
+          },
+          {
+            Lv: 6,
+            Erda: 2,
+            Fragments: 50,
+          },
+          {
+            Lv: 7,
+            Erda: 2,
+            Fragments: 55,
+          },
+          {
+            Lv: 8,
+            Erda: 3,
+            Fragments: 60,
+          },
+          {
+            Lv: 9,
+            Erda: 3,
+            Fragments: 65,
+          },
+          {
+            Lv: 10,
+            Erda: 10,
+            Fragments: 200,
+          },
+          {
+            Lv: 11,
+            Erda: 3,
+            Fragments: 80,
+          },
+          {
+            Lv: 12,
+            Erda: 3,
+            Fragments: 90,
+          },
+          {
+            Lv: 13,
+            Erda: 4,
+            Fragments: 100,
+          },
+          {
+            Lv: 14,
+            Erda: 4,
+            Fragments: 110,
+          },
+          {
+            Lv: 15,
+            Erda: 4,
+            Fragments: 120,
+          },
+          {
+            Lv: 16,
+            Erda: 4,
+            Fragments: 130,
+          },
+          {
+            Lv: 17,
+            Erda: 4,
+            Fragments: 140,
+          },
+          {
+            Lv: 18,
+            Erda: 4,
+            Fragments: 150,
+          },
+          {
+            Lv: 19,
+            Erda: 5,
+            Fragments: 160,
+          },
+          {
+            Lv: 20,
+            Erda: 15,
+            Fragments: 350,
+          },
+          {
+            Lv: 21,
+            Erda: 5,
+            Fragments: 170,
+          },
+          {
+            Lv: 22,
+            Erda: 5,
+            Fragments: 180,
+          },
+          {
+            Lv: 23,
+            Erda: 5,
+            Fragments: 190,
+          },
+          {
+            Lv: 24,
+            Erda: 5,
+            Fragments: 200,
+          },
+          {
+            Lv: 25,
+            Erda: 5,
+            Fragments: 210,
+          },
+          {
+            Lv: 26,
+            Erda: 6,
+            Fragments: 220,
+          },
+          {
+            Lv: 27,
+            Erda: 6,
+            Fragments: 230,
+          },
+          {
+            Lv: 28,
+            Erda: 6,
+            Fragments: 240,
+          },
+          {
+            Lv: 29,
+            Erda: 7,
+            Fragments: 250,
+          },
+          {
+            Lv: 30,
+            Erda: 20,
+            Fragments: 500,
+          },
+        ],
+      },
+    },
+    Developer: "Ninbang @ Scannia/Maplestory(KMS), Ruan-P(GitHub)",
+  };
 }
